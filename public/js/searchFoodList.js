dc9a5ab2a99aa60a8c0ccb2d93f53a80c31b4ba0
Mbaireann <mbaireann12@gmail.com>
2021-04-25 11:29:57 +0300

    fixed pagination of list

--- public/js/searchFoodList.js
+++ public/js/searchFoodList.js
@@ -1,17 +1,16 @@
 window.addEventListener("load", async (event) => {
-
   const options = {
-    item:function(values){
+    item: function (values) {
       // console.log(values)
 
-     return `<li class="list-group-item"><a class="_id text-decoration-none" href="/meal/create/${values._id}"><p class="food_name fs-7 mb-0"></p></a></li>`
+      return `<li class="list-group-item"><a class="_id text-decoration-none" href="/meal/create/${values._id}"><p class="food_name fs-7 mb-0"></p></a></li>`;
     },
     valueNames: ["food_name"],
     page: 15,
     pagination: [
       {
-        innerWindow: 7,
-        outerWindow: 0,
+        innerWindow: 0,
+        outerWindow: 1,
         name: "pagination",
         item:
           ' <li class=" page-item mt-2"><a class="page page-link fs-7" href="#"></a></li>',
