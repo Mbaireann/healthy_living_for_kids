0f9569c688126ba7bd1a15af7baa53332c43939c
Mbaireann <mbaireann12@gmail.com>
2021-04-25 12:32:53 +0300

    updated home style

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,11 @@
+
+
 window.addEventListener("load", async (event) => {
+  let date= document.getElementById("mydate")
+console.log(date)
+  date.addEventListener('input',async(e)=>{
+    console.log(e.target.value)
+  })
   let user_id = document.getElementById("user_id").value;
   const res = await fetch(
     `/meal/${user_id}/stats/2021-04-24T05:40:59.302+00:00`
@@ -15,9 +22,6 @@ window.addEventListener("load", async (event) => {
   let proteinHtmlTag = document.getElementById("protein");
   let fatsHtmlTag = document.getElementById("fats");
   let caloriesHtmlTag = document.getElementById("calories");
-  // let breakfastHtmlTag = document.getElementById("breakfast")
-  // let lunchHtmlTag = document.getElementById("lunch")
-  // let dinnerHtmlTag = document.getElementById("dinner")
 
   console.log(nutrients);
 
@@ -27,10 +31,16 @@ window.addEventListener("load", async (event) => {
   caloriesHtmlTag.innerText = nutrients[0].total_calories;
 
   meal_category.forEach((item) => {
+    const { records } = item;
     console.log(item);
     if (item.meal_category !== null) {
       document.getElementById(`${item.meal_category.toLowerCase()}`).innerText =
         item.total_calories;
+      records.forEach((item) => {
+        document.getElementById(
+          `${item.meal_category.toLowerCase()}_foods`
+        ).innerText = item.food_name[0].food_name;
+      });
     }
   });
 });
